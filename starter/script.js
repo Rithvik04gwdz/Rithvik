8a2fa8d1dbcf9984f802d3832aa9c0045f8b692b
Rithvik Gowda <rithvikgowda30@gmail.com>
2025-12-06 20:14:54 +0530

    Update script.js

--- starter/script.js
+++ starter/script.js
@@ -1,3 +1,11 @@
 document.getElementById("info-btn").addEventListener("click", () => {
-    alert("JS effect working!");
+    const btn = document.getElementById("info-btn");
+
+    btn.style.transform = "scale(0.9)";
+    btn.style.opacity = "0.7";
+
+    setTimeout(() => {
+        btn.style.transform = "scale(1)";
+        btn.style.opacity = "1";
+    }, 150);
 });
